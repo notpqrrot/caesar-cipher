cfb408f2633196ba26e186d3f92102767b58dece
notpqrrot <167501396+notpqrrot@users.noreply.github.com>
2024-06-11 16:14:03 -0700

    Update cipher.py

--- cipher.py
+++ cipher.py
@@ -1,6 +1,6 @@
 import time
 import random
-import os
+
 alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 def encrypt():
     newstr = ''
